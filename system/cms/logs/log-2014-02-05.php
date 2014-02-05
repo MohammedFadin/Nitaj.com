aff3515dd5365be6933b04b1cc789ba74ce708b7
Mohammed Fadin <imohammad@me.com>
2014-02-05 21:14:37 +0300

    error

--- system/cms/logs/log-2014-02-05.php
+++
@@ -1,5 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2014-02-05 17:37:51 --> Plugin method "options" does not exist on class "Plugin_Theme".
-ERROR - 2014-02-05 17:37:56 --> Plugin method "options" does not exist on class "Plugin_Theme".
-ERROR - 2014-02-05 17:37:58 --> Plugin method "options" does not exist on class "Plugin_Theme".
