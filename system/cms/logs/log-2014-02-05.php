16c3c42b6a5ed2d172fbbbd9c4478bce04363121
Mohammed S Fadin <imohammad@me.com>
2014-02-06 00:11:38 +0300

    updated project header

---
+++ system/cms/logs/log-2014-02-05.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-02-05 20:29:06 --> Severity: Notice  --> Undefined index: name /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/plugins/user.php 359
+ERROR - 2014-02-05 20:55:57 --> Severity: Notice  --> Undefined index: loggin_in /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/plugins/user.php 359
+ERROR - 2014-02-05 21:00:10 --> Severity: Notice  --> Undefined index: user:login_btn /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/themes/Projectvirals/views/partials/header.html 29
+ERROR - 2014-02-05 21:18:15 --> Page Missing: users/signup
+ERROR - 2014-02-05 21:18:24 --> Page Missing: users/signup
+ERROR - 2014-02-05 21:31:10 --> Page Missing: edit_profile
+ERROR - 2014-02-05 21:31:27 --> Page Missing: edit_profile
+ERROR - 2014-02-05 21:41:03 --> Severity: Notice  --> Undefined index: has_cp_premission /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/plugins/user.php 359
