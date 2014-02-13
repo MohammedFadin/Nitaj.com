7440e5fd72f66a908e58b2dff2f80498ab995a26
Mohammed S Fadin <imohammad@me.com>
2014-02-13 11:48:02 +0300

    Added Login Form
    All login forms design and resources are added

--- system/cms/logs/log-2014-02-06.php
+++ system/cms/logs/log-2014-02-06.php
@@ -4,3 +4,12 @@ ERROR - 2014-02-06 00:10:47 --> Severity: Notice  --> Trying to get property of 
 ERROR - 2014-02-06 00:10:47 --> Severity: Notice  --> Trying to get property of non-object /Users/iMohammad/Github/senior-project/system/cms/themes/Projectvirals/views/modules/blog/view.php 47
 ERROR - 2014-02-06 00:10:47 --> Severity: Notice  --> Trying to get property of non-object /Users/iMohammad/Github/senior-project/system/cms/themes/Projectvirals/views/modules/blog/view.php 48
 ERROR - 2014-02-06 00:10:47 --> Severity: Notice  --> Trying to get property of non-object /Users/iMohammad/Github/senior-project/system/cms/themes/Projectvirals/views/modules/blog/view.php 54
+ERROR - 2014-02-06 10:57:44 --> Severity: Notice  --> Undefined property: CI::$user /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/libraries/MX/Loader.php 276
+ERROR - 2014-02-06 10:57:44 --> Severity: Notice  --> Trying to get property of non-object /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/themes/Projectvirals/views/partials/header.html 15
+ERROR - 2014-02-06 10:57:58 --> Severity: Notice  --> Undefined property: CI::$current_id /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/libraries/MX/Loader.php 276
+ERROR - 2014-02-06 10:58:14 --> Severity: Notice  --> Undefined property: CI::$current_urser /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/libraries/MX/Loader.php 276
+ERROR - 2014-02-06 10:58:17 --> Severity: 4096  --> Object of class stdClass could not be converted to string /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/themes/Projectvirals/views/partials/header.html 15
+ERROR - 2014-02-06 10:58:31 --> Severity: 4096  --> Object of class stdClass could not be converted to string /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/themes/Projectvirals/views/partials/header.html 15
+ERROR - 2014-02-06 10:58:49 --> Severity: Notice  --> Undefined property: stdClass::$user_name /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/themes/Projectvirals/views/partials/header.html 15
+ERROR - 2014-02-06 10:59:48 --> Severity: Notice  --> Undefined property: stdClass::$name /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/themes/Projectvirals/views/partials/header.html 15
+ERROR - 2014-02-06 10:59:53 --> Severity: Notice  --> Undefined property: stdClass::$user_name /Users/mohammedfadin/Dropbox/GitHub/senior-project/system/cms/themes/Projectvirals/views/partials/header.html 15
--- system/cms/themes/Projectvirals/views/modules/users/login.php
+++ system/cms/themes/Projectvirals/views/modules/users/login.php
@@ -1,7 +1,48 @@
+<link rel="stylesheet" href="system/cms/themes/Projectvirals/css/clear_forms/less/styles.css" type="text/css" />           
+    <div class="block blur2">
+        <div class="container">
+            <div class="row">
+                <div class="span12">
+                    <div class="clear-form two-col">
+                        <div class="form-heading">
+                            <h3 class="header">Sign In</h3>
+                            <hr/>
+                        </div>
+                        <div class="col1">                            
+                            <h4>Use other accounts</h4>
+                            <p>
+                                You can also sign in using your Facebook Account or Google Account
+                            </p>
+                            <a href="" class="btn btn-large btn-block btn-fb">Login with Facebook</a>
+                            <a href="" class="btn btn-large btn-block btn-google">Login with Google</a>
+                        </div>
+                        <div class="col2">
+                            <div class="form-heading">
+                                <h4 class="header">Using your account</h4>                                                        
+                            </div>  
+                            <div class="form-body">                              
+                                <div class="pair-group">
+                                    <input type="text" class="input-block-level" placeholder="Email address">
+                                    <input type="password" class="input-block-level" placeholder="Password">         
+                                </div>
+                           </div>                                 
+                            <div class="form-footer">     
+                                <button class="btn btn-large btn-blue btn-block" type="submit">Sign In</button> 
+                                <p class="center">
+                                    <a href="#">Forgot your password?</a>
+                                </p>
+                            </div>    
+                        </div>          
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- 
 <section id="login">
 	<div class="row-fluid">
 		<?php if (validation_errors()): ?>
-		<!-- Woops... -->
 		<div class="row-fluid">
 		    <div class="span12">
 		      <div class="alert alert-error">
@@ -49,4 +90,4 @@
 		<?php echo form_close(); ?>
 		</div>
 	</div>
-</section>
+</section> -->
